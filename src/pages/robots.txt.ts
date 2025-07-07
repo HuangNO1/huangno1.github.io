905cd8a4f8428b49a80a96c1054a4620fcb55100
Rem <fh831.cp9gw@gmail.com>
2025-07-07 14:43:24 +0800

    try fix robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,7 +3,6 @@ import type { APIRoute } from "astro";
 const robotsTxt = `
 User-agent: *
 Allow: /
-Disallow: /_astro/
 
 Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
 `.trim();
