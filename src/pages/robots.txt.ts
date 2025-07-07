c58aacabd8c7f9827d6f6d84adb0475b4aad2c88
Rem <fh831.cp9gw@gmail.com>
2025-07-07 14:59:18 +0800

    resume robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,6 +3,7 @@ import type { APIRoute } from "astro";
 const robotsTxt = `
 User-agent: *
 Allow: /
+Disallow: /_astro/
 
 Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
 `.trim();
